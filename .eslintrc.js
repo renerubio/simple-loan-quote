07d90d477794c1aeaa78c01b6d684048a23abdce
René Rubio Muñoz <rrrm_2@hotmail.com>
2021-08-03 18:37:27 +0200

    eslint hooks - https://renerubio.atlassian.net/browse/LQ-3

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-hooks"
+    ],
+    "rules": {
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        "react/prop-types": 0
+    }
+};
