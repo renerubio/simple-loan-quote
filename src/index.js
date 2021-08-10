a1c89508095cdd25fd378f2aa3ac139f7e74b8ee
René Rubio Muñoz <rrrm_2@hotmail.com>
2021-08-11 01:13:44 +0200

    style: :art: import css from index.scss
    resolve #10

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
-import './skeleton.scss';
 import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
