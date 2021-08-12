a6c5ecc117e9fa9de954915fc41d7184f9d03172
René Rubio Muñoz <rrrm_2@hotmail.com>
2021-08-13 00:33:17 +0200

    feat: :art: semantic code translation changed to English language
    close #11

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Header = ({ titulo, descripcion }) => (
+const Header = ({ title, descripcion }) => (
   <>
-    <h1>{titulo}</h1>
+    <h1>{title}</h1>
     <p className="color-white">{descripcion}</p>
   </>
 );
